398666d314f887b60a6713599fea41db73f0e553
Krzysztof Kamieniecki <krzysztof@thinkstudio.pl>
2018-11-22 13:51:16 +0100

    react-hotloader plugin added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { hot } from 'react-hot-loader';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Grab that stuff !!! </h1>
+        <h1> Grab that stuff !!!</h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
